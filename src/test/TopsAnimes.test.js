ccd08a10fed2315bdd052b2575893109a91c7a68
Erickvergara97 <81184269+Erickvergara97@users.noreply.github.com>
2021-06-22 13:26:07 -0500

    build deleted

--- src/test/TopsAnimes.test.js
+++ src/test/TopsAnimes.test.js
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/react'
 import { render } from '@testing-library/react';
 import TopsAnimes from "../components/TopsAnimes";
+import {shallow} from "enzyme"
 
 
 const dispatchMock = jest.fn();
@@ -11,9 +12,7 @@ const dispatchMock = jest.fn();
 
 test('render TopsAnimes', () => {
     const topanime =[];
-    const scrolltop = jest.fn(
-        window.scrollTo(0,0)
-    )
+    const scrolltop = jest.fn()
     const component = render(
     <TopsAnimes 
     topanime={topanime}
@@ -27,13 +26,29 @@ test('render TopsAnimes', () => {
 
 test('render link', () => {
     const topanime =[];
-    const scrolltop = jest.fn(
-        window.scrollTo(0,0)
-    )
+    const scrolltop = jest.fn()
     const component = render(<TopsAnimes 
         topanime={topanime}
         key={anime.mal_id}
         scrolltop={scrolltop} 
         />)
     const link = component.container.querySelector('Link')
-  })
+    expect.arrayContaining(topanime)
+})
+
+describe('SomeComponent', () => {
+    test('should handle click', () => {
+        const topanime =[];
+        const scrolltop = jest.fn()
+        const wrapper = shallow(<TopsAnimes 
+        topanime={topanime}
+        key={anime.mal_id}
+        scrolltop={scrolltop} 
+        />);
+        const mEvent = { preventDefault: jest.fn() };
+        window.scrollTo = jest.fn();
+        wrapper.find('button').simulate('click', mEvent);
+        expect(mEvent.preventDefault).toBeCalled();
+        expect(window.scrollTo).toBeCalledWith(0, 0);
+    });
+});
